d8552e0310086db92f8285db4372bb2032d042cd
MDP1997 <94741262+MDP1997@users.noreply.github.com>
2021-12-16 18:23:08 +0000

    Okteto Change

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 require('dotenv').config();
+const req = require('express/lib/request');
 const app = require('./appmateria');
 require('./database');
-
+var BASE_API_PATH = "/api/v1";
 async function main() {
     await app.listen(app.get('port'));
     console.log('Server up on port: ', app.get('port'));
 }
+
+app.get(BASE_API_PATH + "/healthz", (req, res) => {
+    
+    res.sendStatus(200);
+
+});
 
 
 main();
