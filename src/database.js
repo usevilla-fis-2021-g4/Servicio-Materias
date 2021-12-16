3bcfadb72e401b54168b8a9d56389155a9ebe572
MDP1997 <94741262+MDP1997@users.noreply.github.com>
2021-12-16 19:11:49 +0000

    Cambio MongoDB Despliegue

--- src/database.js
+++ src/database.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
-const URI = "mongodb://localhost:27017/materiasDB";
+//const URI = "mongodb://localhost:27017/materiasDB";
+const URI = "mongodb://mongodb:27017";
 
 mongoose.connect(URI);
 
