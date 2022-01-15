1ac7f1b1292a72c5130f57dff8e31f4de3f50814
Miguel Diaz <bladdenemes@gmail.com>
2022-01-15 21:00:16 +0000

    Routes With Api Auth

--- src/routes/materias.js
+++ src/routes/materias.js
@@ -4,12 +4,12 @@ const router = Router();
 const { getMaterias, createMateria, getMateria, deleteMateria, updateMateria } = require('../controllers/materia.controller');
 
 router.route('/')
-    .get(getMaterias)
-    .post(createMateria);
+ .get(getMaterias)
+ .post(passport.authenticate("localapikey", {session: false}),createMateria);
 
 router.route('/:id')
-    .get(getMateria)
-    .delete(deleteMateria)
-    .put(updateMateria);
+ .get(getMateria)
+ .delete(passport.authenticate("localapikey", {session: false}),deleteMateria)
+ .put(passport.authenticate("localapikey", {session: false}),updateMateria);
 
 module.exports = router;
