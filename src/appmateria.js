10e2d5df498c6eb3669f9fcf1bc0f589e38543ea
Miguel Diaz <bladdenemes@gmail.com>
2022-01-15 20:59:01 +0000

    Apikey Running on appmateria

--- src/appmateria.js
+++ src/appmateria.js
@@ -1,16 +1,42 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config();
+var bodyParser = require("body-parser");
+const passport = require('passport');
+require('../passport');
 
+//
 const app = express();
 
+//Lectura y Parseo del body
+app.use(express.json({limit: "50mb"}));
+app.use(bodyParser.urlencoded({limit: "50mb", extended: true, parameterLimit:50000}));
+
+
+
 // settings
-app.set('port', process.env.PORT || 1600);
+app.set('port', process.env.PORT || 2999);
 
 //middlewares 
 app.use(cors());
 app.use(express.json());
 
 //routes
-app.use('/api/materias', require('./routes/materias'));
+app.use('/apimaterias/v1/materias', require('./routes/materias'));
+
+//Uso del passport
+app.use(passport.initialize());
+
+//server
+
+app.get("/", (request, response) => {
+    response.send("<html><body><h1>My Server.</h1></body></html>");
+});
+
+app.get("/apimaterias/v1/healthz", (req, res) => {
+    
+    res.sendStatus(200);
+
+});
 
 module.exports = app;
