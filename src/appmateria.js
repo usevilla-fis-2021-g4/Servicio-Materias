4822fa5fba10d816920f7df48a9192c1d1b73136
Miguel Diaz <bladdenemes@gmail.com>
2022-01-15 20:59:17 +0000

    Apikey Change on apimateria

--- src/appmateria.js
+++ src/appmateria.js
@@ -12,8 +12,6 @@ const app = express();
 app.use(express.json({limit: "50mb"}));
 app.use(bodyParser.urlencoded({limit: "50mb", extended: true, parameterLimit:50000}));
 
-
-
 // settings
 app.set('port', process.env.PORT || 2999);
 
